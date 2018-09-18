5c0ea9f3fb7d891da9068667e540c89f2705df7e
GT20176071\user <nkc.shunta.kawai@gmail.com>
2018-09-18 15:34:36 +0900

    敵が球に向かうようにした

--- 20180914/Assets/Shunta'sScript/enemycontroller.cs
+++ 20180914/Assets/Shunta'sScript/enemycontroller.cs
@@ -2,24 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playercontroller : MonoBehaviour {
+public class enemycontroller : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public GameObject target;
+    public float moveSpeed = 1.0f;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            transform.Translate(0f, 0f, 1.0f);
-        }
-
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            transform.Translate(0f, 0f, -1.0f);
-        }
+        // 常にターゲットに向かう ---
+        // 2点のベクトルを取得
+        Vector3 dir = target.transform.position - transform.position;
+        // 正規化
+        dir.Normalize();
+        // 移動させる
+        transform.Translate(dir.x * moveSpeed, dir.y * moveSpeed, 0.0f);
 
     }
 }
