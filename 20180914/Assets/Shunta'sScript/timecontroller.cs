8326e26fbf3e70ec67c73bb3dd7a997c76bed0bf
GT20176071\user <nkc.shunta.kawai@gmail.com>
2018-09-18 10:47:07 +0900

    時間実装

--- 20180914/Assets/Shunta'sScript/timecontroller.cs
+++ 20180914/Assets/Shunta'sScript/timecontroller.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class timecontroller : MonoBehaviour {
-
+    //time変数
+    private float time;
+    public Text timelabel;
 	// Use this for initialization
 	void Start () {
-		
+        //制限時間
+        time = 10;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        //時間表示
+        timelabel.text = string.Format("{0:000}", time);
+
+        //減少
+        time -= Time.deltaTime;
+
+        //０なら０のまま
+        if (time <= 0.0f)
+        {
+            time = 0;
+        }
 	}
 }
