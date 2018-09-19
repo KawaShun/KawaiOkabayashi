bc5262a19aa076a34c0aa5c36b0ede34a331f6f7
Kota.Okabayadhi <nkc.kota.okabayashi@gmail.com>
2018-09-19 10:07:24 +0900

    個人で用意したデータ(kota's part)の追加

---
+++ 20180914/Assets/kota's part/ball.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ball : MonoBehaviour {
+
+    public GameObject player1;
+    int hit = 0;
+    Vector3 t  ;
+   public Vector3 b=new Vector3(0,0,0);
+    Vector3 p ;
+    Vector3 force;
+    public Rigidbody rb;
+
+    public   float power = 0;
+    void Start () {
+        rb = this.GetComponent<Rigidbody>();
+
+    
+    }
+
+
+
+   
+    void Update () {
+        
+        
+        power =(Mathf.Abs(power*0.3f));
+      //  float x = Input.GetAxis("Horizontal") * power*6;
+      //  float y = Input.GetAxis("Vertical") * power*6;
+    //    rb.AddForce(x, 0, y);
+
+
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            hit = 1;
+            t = player1.GetComponent<Transform>().position;
+            b = this.transform.position;
+            p = b - t;
+            power = 0;
+            power = player1.GetComponent<mov>().power;
+            Debug.Log("hit");
+            shoot();
+        }
+        if (other.gameObject.tag == "wall")
+        {
+            Debug.Log("衝突");
+           // force = new Vector3(p.x, p.y, -p.z);
+           // rb.AddForce(force * power, ForceMode.Impulse);
+        }
+    }
+    void shoot()
+    {
+        Rigidbody rb = this.GetComponent<Rigidbody>();  // rigidbodyを取得
+         force = new Vector3(p.x, p.y, p.z);  // 力を設定
+        rb.AddForce(force*power, ForceMode.Impulse);
+
+    }
+}
---
+++ 20180914/Assets/kota's part/mov.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mov : MonoBehaviour
+{
+    public float power = 10;
+    bool woaking = true;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) && woaking == true)
+        {
+            this.transform.Translate(-0.2f, 0, 0);
+
+        }
+        if (Input.GetKey(KeyCode.RightArrow) && woaking == true)
+        {
+            this.transform.Translate(0.2f, 0, 0);
+
+        }
+        if (Input.GetKey(KeyCode.UpArrow) && woaking == true)
+        {
+            this.transform.Translate(0, 0, 0.2f);
+
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && woaking == true)
+        {
+            this.transform.Translate(0, -0, -0.2f);
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "ball")
+        {
+          
+        }
+    }
+}
