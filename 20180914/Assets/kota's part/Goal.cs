1b7e7a89ebfd1297b0af07a4052f90632948d0e8
Kota.Okabayadhi <nkc.kota.okabayashi@gmail.com>
2018-09-28 12:09:35 +0900

    ゴールの機能、スコア加点の追加

---
+++ 20180914/Assets/kota's part/Goal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Goal : MonoBehaviour
+{
+
+    public int idd = 2;
+    public GameObject Director;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "ball")
+        {
+            Debug.Log("in");
+            Destroy(other.gameObject);
+          //  Director.GetComponent<Director>().id = 2;
+            Director.GetComponent<Director>().Addcount(idd);
+        }
+        // Debug.Log("Goal");
+
+    }
+
+
+}
